862d50bbf42f464ca8f68f5d2318ab00b0fe74dd
Quentin Rapilly <qrapilly@calcarine.irisa.fr>
2022-05-04 10:57:30 +0200

    [*]correction du script de regroupement des segs ASHS

--- code/ASHS/segmentation_auto/group_seg_labels.py
+++ code/ASHS/segmentation_auto/group_seg_labels.py
@@ -10,12 +10,13 @@ if __name__ == "__main__":
     assert(len(sys.argv) > 2)
     in_dir, out_dir = sys.argv[1], sys.argv[2]
     for dir in listdir(in_dir):
-        if isdir(dir):
-            current_dir = join(in_dir, dir)
+        current_dir = join(in_dir, dir)
+        if isdir(current_dir) and dir!="workspace" :
             files = listdir(current_dir)
             i = 0
             while files[i].find("layer_002_") != 0 :
                 i += 1
-            
-            cp(join(current_dir,files[i]), join(out_dir,dir+".nii.gz"))
+            good_file = join(current_dir, files[i])
+            print(good_file)
+            cp(good_file, join(out_dir,dir+".nii.gz"))
 
