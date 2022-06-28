5c6c9ebe702e507d21ced07ac363083122042845
QuentinRapilly <quentin.rapilly@ensta-paris.fr>
2022-06-28 11:36:05 +0200

    [*]Gestion des labels droite gauche FSL

--- code/segmentation/FSL/fsl_seg.py
+++ code/segmentation/FSL/fsl_seg.py
@@ -32,18 +32,26 @@ def all_fsl_seg(input_dir, output_dir):
             print("Processing file : {}".format(filename))
             fsl_seg(join(input_dir,filename), tmp_dir)
     
-    seg_only = join(output_dir,"seg_only")
-    mkdir(seg_only)
+    seg_only_left = join(output_dir,"seg_only_left")
+    mkdir(seg_only_left)
+
+    seg_only_right = join(output_dir,"seg_only_right")
+    mkdir(seg_only_right)
 
     print("## Copying the segmentation in a dedicated dir")
     for dirname in listdir(output_dir):
-        if dirname != "seg_only":
+        if dirname.find("seg_only") >=0 :
             current_dir = join(output_dir, dirname)
 
             for filename in listdir(current_dir):
-                if filename.find("first.nii.gz") > -1:
+                if filename.find("L_Hipp_first.nii.gz") > -1:
                     to_copy = join(current_dir, filename)
-                    destination = join(seg_only, dirname+".nii.gz")
+                    destination = join(seg_only_left, dirname+".nii.gz")
+                    copy2(to_copy, destination)
+
+                if filename.find("R_Hipp_first.nii.gz") > -1:
+                    to_copy = join(current_dir, filename)
+                    destination = join(seg_only_right, dirname+".nii.gz")
                     copy2(to_copy, destination)
 
 
