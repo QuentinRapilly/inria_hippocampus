9cf7ab9d26a58c46d6c16c99719f32d10b6ce45c
QuentinRapilly <quentin.rapilly@ensta-paris.fr>
2022-06-29 09:38:15 +0200

    [*]Ajout de commentaires dans la plupart des scripts

--- code/shape_analysis/db_registration.py
+++ code/shape_analysis/db_registration.py
@@ -6,6 +6,13 @@ from os import listdir, mkdir
 from os.path import join, isdir, splitext, basename
 
 class MeanSpaceRegister():
+
+    """
+        This class is inspired by the one defined in the iterativ_barycentre.py file.
+        Here are defined every functions necessary to register the shapes in our database
+        to the mean shape space (computed by the iterative barycentre algorithm).
+    """
+
     def __init__(self, input_path, output_path, mean_shape, config_file=None, verbose = False,  logdir = './logdir') -> None:
         
         self.registration_dir = join(output_path, "registration")
@@ -23,23 +30,34 @@ class MeanSpaceRegister():
 
     
     def reload_config(self):
+        """
+            The config dict is reloaded after every internal modification to avoid any mistake
+            caused by a saved modification that shoudn't be.
+        """
         with open(self.config_file,"r") as f:
             self.config = json.load(f)
 
     def init_register(self, dir):
+        """
+            Creates a Deformetrica object (that can call the API) with the good result storing
+            directory after each step. 
+        """
         self.dfca = Deformetrica(output_dir=dir, verbosity='ERROR')
 
-    def register(self, shape1, shape2, dir):
+    def register(self, mean_shape, shape, dir):
+        """
+            Applies the registration operation from the mean shape to the choosen shape.
+        """
         self.reload_config()
         cfg = self.config["registration"].copy()
 
         # template_specification dic modifications
         template_spec = cfg["template_specification"]
-        template_spec["shape"]["filename"] = shape1
+        template_spec["shape"]["filename"] = mean_shape
 
         # dataset_options dic modifications
         dataset_spec = cfg["dataset_specification"]
-        dataset_spec["dataset_filenames"].append([{"shape" : shape2}])
+        dataset_spec["dataset_filenames"].append([{"shape" : shape}])
         dataset_spec["subject_ids"].append("target")
 
         model_spec = cfg["model_options"]
@@ -52,6 +70,10 @@ class MeanSpaceRegister():
         
     
     def register_db(self):
+
+        """
+            Applies the registration process for every file in the input directory.
+        """
         
         for filename in self.shapes:
 
@@ -70,10 +92,10 @@ class MeanSpaceRegister():
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-i", "--input", help="")
-    parser.add_argument("-o", "--output", help="")
-    parser.add_argument("-m", "--mean_shape", help="")
-    parser.add_argument("-c", "--config", help="")
+    parser.add_argument("-i", "--input", help="Input dir containing the files to register")
+    parser.add_argument("-o", "--output", help="Output file where to store the results")
+    parser.add_argument("-m", "--mean_shape", help="Path to the mean shape")
+    parser.add_argument("-c", "--config", help="Config file")
 
     args = parser.parse_args()
 
