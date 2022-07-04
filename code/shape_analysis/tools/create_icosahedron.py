925a1195e269fb0913f5df927cef49b6de74ce81
QuentinRapilly <quentin.rapilly@ensta-paris.fr>
2022-07-04 16:59:57 +0200

    [*]Petite modif icosahedron 3

--- code/shape_analysis/tools/create_icosahedron.py
+++ code/shape_analysis/tools/create_icosahedron.py
@@ -18,8 +18,8 @@ if __name__ == "__main__" :
     space = pv.read(args.space)
     x, X, y, Y, z, Z = space.GetBounds()
     bounds = np.array([[x,X],[y,Y],[z,Z]])
-    gamma = np.max(bounds)/2
-    middle = (bounds[:,0]+bounds[:,1])/4
+    gamma = np.max(bounds)/4
+    middle = (bounds[:,0]+bounds[:,1])/2
     points = gamma*points + middle
 
     edges_modified = np.hstack((3*np.ones((edges.shape[0],1),dtype=int),edges)) 
