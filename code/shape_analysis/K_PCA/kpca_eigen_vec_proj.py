9f3174e8a10ce797e9f7593556bb1f86621665a0
QuentinRapilly <quentin.rapilly@ensta-paris.fr>
2022-08-03 16:31:22 +0200

    [+] plot des vecteurs propres v9

--- code/shape_analysis/K_PCA/kpca_eigen_vec_proj.py
+++ code/shape_analysis/K_PCA/kpca_eigen_vec_proj.py
@@ -7,13 +7,25 @@ from ntpath import basename
 
 from kpca_tools import manage_momenta, manage_control_points, compute_kernel, expand_kernel
 
+COLORS = {"ashs":"blue", "fsl":"res", "g_truth":"green"}
 
-def get_subject_color(filename):
+def get_subject_id(filename):
     splitted = filename.split("_")
     id = splitted[-4]+splitted[-2]
+
+    return id
+
+def get_subject_color(filename):
+    id = get_subject_id(filename)
     r,g,b = (int(id[:3])/140)*0.6+0.4, (int(id[3:5])/100)*0.6+0.4, (int(id[5:])/100)*0.6+0.4
 
     return np.array([r,g,b])
+
+def get_used_method(filename):
+    if filename.find("ashs")>=0: return "ashs"
+    if filename.find("fsl")>=0: return "fsl"
+    if filename.find("g_truth")>=0: return "g_truth"
+
 
 def compute_proj(momenta_files, control_points, eigen, std, dims_to_keep, output = "out.png", verbose = False):
     eigen_dic = np.load(eigen)
@@ -36,6 +48,8 @@ def compute_proj(momenta_files, control_points, eigen, std, dims_to_keep, output
     x = proj[:,0]
     y = proj[:,1]
 
+    ## METHOD DE PRINT 1
+    """
     colors_list = list()
 
     ashs_idx, fsl_idx, g_truth_idx = [], [], []
@@ -62,11 +76,35 @@ def compute_proj(momenta_files, control_points, eigen, std, dims_to_keep, output
     # Scatter pour ASHS
     if verbose :
         print("x : {}\ny : {}\ncolors:{}".format(x[ashs_idx], y[ashs_idx], colors[ashs_idx]))
-    plt.scatter(x=x[ashs_idx], y=y[ashs_idx], c=colors[ashs_idx], marker='o', linestyle = "--")
+    plt.scatter(x=x[ashs_idx], y=y[ashs_idx], c=colors[ashs_idx], marker='o')
     # Scatter pour FSL
     plt.scatter(x=x[fsl_idx], y=y[fsl_idx], c=colors[fsl_idx], marker='v')
     # Scatter pour G_TRUTH
     plt.scatter(x=x[g_truth_idx], y=y[g_truth_idx], c=colors[g_truth_idx], marker='+')
+
+    """
+
+    ## FIN DE LA METHODE DE PRINT 1
+
+    ## METHODE DE PRINT 2
+
+    print_dic = {}
+
+    for i,filename in enumerate(momenta_files) :
+        real_name = basename(filename)
+        id = get_subject_id(real_name)
+        method = get_used_method(real_name)
+
+        res = print_dic.get(id)
+        if res == None:
+            print_dic[id] = {"x":[x[i]], "y":[y[i]], "c":[COLORS[method]], "linestyle":"--"}
+        else:
+            res["x"].append(x[i])
+            res["y"].append(y[i])
+            res["c"].append(COLORS[method])
+
+    for key in print_dic:
+        plt.plot(x="x", y="y", c="c", linestyle="linestyle", data=print_dic[key])
 
     plt.savefig(output)
 
