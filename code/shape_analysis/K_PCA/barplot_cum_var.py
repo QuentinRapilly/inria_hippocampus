3a0123eaf6e000b46c330edba77f095b04373956
QuentinRapilly <quentin.rapilly@ensta-paris.fr>
2022-08-29 15:51:19 +0200

    [+]Cumulative variance barplot

---
+++ code/shape_analysis/K_PCA/barplot_cum_var.py
@@ -0,0 +1,29 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import argparse
+
+
+if __name__=="__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input")
+    parser.add_argument("-o", "--output")
+
+    args = parser.parse_args()
+
+    input, output = args.input, args.output
+
+    data = np.load(input)
+    eigen_values = data["eigen_values"]
+    y = 100*eigen_values/np.sum(eigen_values)
+    y = np.cumsum(y)
+    
+    plt.bar(x=[i for i in range(1, len(y)+1)], height=y)
+    plt.title("Explained variance (%) according to the number of dim taken into account")
+    plt.xlabel("Number of dimension")
+    plt.ylabel("Variance explained (%)")
+    plt.legend()
+
+    plt.savefig(output)
+
+    
