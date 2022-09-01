8cd5a3e4c5bc981a157a4f892a6138406255d25f
QuentinRapilly <quentin.rapilly@ensta-paris.fr>
2022-09-01 16:07:30 +0200

    [+]Volume code

--- code/shape_analysis/tools/volume_dir.sh
+++ code/shape_analysis/tools/volume_dir.sh
@@ -4,6 +4,9 @@ input_dir=$1
 output_dir=$2
 volume_file=${output_dir}/volumes.txt
 
+crt_path=pwd
+path=$(dirname "$crt_path")
+
 for filename in "${input_dir}"; do
-    bash volume_single_file.sh "${input_dir}/${filename}" "${output_dir}" >>  "${volume_file}"
+    bash "${path}"volume_single_file.sh "${input_dir}/${filename}" "${output_dir}" >> "${volume_file}"
 done
